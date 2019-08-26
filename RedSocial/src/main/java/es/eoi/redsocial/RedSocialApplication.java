5a1efc6c0ea724bbb7362e1af80109766a6f101c
Franziskeer <fpa19@alu.ua.es>
2019-08-26 15:53:54 +0200

    Primer commit

---
+++ RedSocial/src/main/java/es/eoi/redsocial/RedSocialApplication.java
@@ -0,0 +1,21 @@
+package es.eoi.redsocial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+@EnableJpaRepositories(basePackages = "es.eoi.redsocial")
+@EntityScan( basePackages = {"es.eoi.redsocial"} )
+@ComponentScan(basePackages = "es.eoi.redsocial")
+@EnableAutoConfiguration
+public class RedSocialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedSocialApplication.class, args);
+	}
+
+}
